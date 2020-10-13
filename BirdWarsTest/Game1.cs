93899125bdf9b33598c723adef360b6f245103a0
Avival <31579334+Avival-Code@users.noreply.github.com>
2020-10-13 16:40:07 -0500

    Added Basic Architecture -  GameStates, StateHandler. Fixed random readbility issues.

--- BirdWarsTest/Game1.cs
+++ BirdWarsTest/Game1.cs
@@ -12,7 +12,7 @@ namespace BirdWarsTest
 
 		public Game1()
 		{
-			_graphics = new GraphicsDeviceManager(this);
+			_graphics = new GraphicsDeviceManager( this );
 			Content.RootDirectory = "Content";
 			IsMouseVisible = true;
 		}
@@ -24,24 +24,25 @@ namespace BirdWarsTest
 
 		protected override void LoadContent()
 		{
-			_spriteBatch = new SpriteBatch(GraphicsDevice);
+			_spriteBatch = new SpriteBatch( GraphicsDevice );
 		}
 
-		protected override void Update(GameTime gameTime)
+		protected override void Update( GameTime gameTime )
 		{
-			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+			if ( GamePad.GetState( PlayerIndex.One ).Buttons.Back == ButtonState.Pressed || 
+				 Keyboard.GetState().IsKeyDown( Keys.Escape ) )
 				Exit();
 			base.Update(gameTime);
 		}
 
-		protected override void Draw(GameTime gameTime)
+		protected override void Draw( GameTime gameTime )
 		{
-			GraphicsDevice.Clear(Color.CornflowerBlue);
+			GraphicsDevice.Clear( Color.CornflowerBlue );
 
 			_spriteBatch.Begin();
 			_spriteBatch.End();
 
-			base.Draw(gameTime);
+			base.Draw( gameTime );
 		}
 	}
 }
