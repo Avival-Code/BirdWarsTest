4311f30a5412db95b24d5228b6a4ad53a7f86e07
Avival <31579334+Avival-Code@users.noreply.github.com>
2020-10-13 16:47:51 -0500

    Added states to Statehandler class

--- BirdWarsTest/States/StateHandler.cs
+++ BirdWarsTest/States/StateHandler.cs
@@ -8,9 +8,16 @@ namespace BirdWarsTest.States
 {
 	class StateHandler
 	{
-		public StateHandler( Microsoft.Xna.Framework.Content.ContentManager content )
+		public StateHandler( ref Microsoft.Xna.Framework.Content.ContentManager content )
 		{
-			gameStates = new GameState[ 6 ];
+			gameStates = new GameState[ 7 ];
+			gameStates[ 0 ] = new MainAnimationState( ref content );
+			gameStates[ 1 ] = new MainMenuState( ref content );
+			gameStates[ 2 ] = new WaitingRoomState( ref content );
+			gameStates[ 3 ] = new PlayState( ref content );
+			gameStates[ 4 ] = new OptionsState( ref content );
+			gameStates[ 5 ] = new UserRegistryState( ref content );
+			gameStates[ 6 ] = new StatisticsState(ref content);
 		}
 
 		public void ChangeState( StateTypes state )
