76e3a3811a3804e17e5b6bef91425c2aa1ce25b9
Avival <31579334+Avival-Code@users.noreply.github.com>
2020-10-19 21:42:26 -0500

    Added InputComponent abstract class and PlayerInputComponent class.

---
+++ BirdWarsTest/InputComponents/InputComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BirdWarsTest.GameObjects;
+using Microsoft.Xna.Framework.Input;
+
+namespace BirdWarsTest.InputComponents
+{
+	abstract class InputComponent
+	{
+		abstract public void HandleInput( KeyboardState state, GameObject gameObject );
+
+		protected Command moveUpButton = null;
+		protected Command moveDownButton = null;
+		protected Command moveLeftButton = null;
+		protected Command moveRightButton = null;
+	}
+}
---
+++ BirdWarsTest/InputComponents/PlayerInputComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BirdWarsTest.GameObjects;
+using Microsoft.Xna.Framework.Input;
+
+namespace BirdWarsTest.InputComponents
+{
+	class PlayerInputComponent: InputComponent
+	{
+		public PlayerInputComponent()
+		{
+			moveUpButton = new MoveUpCommand();
+			moveDownButton = new MoveDownCommand();
+			moveLeftButton = new MoveLeftCommand();
+			moveRightButton = new MoveRightCommand();
+		}
+
+		public override void HandleInput( KeyboardState state, GameObject gameObject )
+		{
+			if ( state.IsKeyDown( Keys.Up ) ) moveUpButton.execute( gameObject );
+			else if ( state.IsKeyDown( Keys.Down ) ) moveDownButton.execute( gameObject );
+			else if ( state.IsKeyDown( Keys.Left ) ) moveLeftButton.execute( gameObject );
+			else if ( state.IsKeyDown( Keys.Right ) ) moveRightButton.execute( gameObject );
+		}
+	}
+}
