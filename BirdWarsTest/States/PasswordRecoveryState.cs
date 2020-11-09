412885159f22e2a5dfcb1a6011fd483caf58f15b
Avival <31579334+Avival-Code@users.noreply.github.com>
2020-11-08 22:53:29 -0600

    Added gameObjects to PasswordRecoveryState.

--- BirdWarsTest/States/PasswordRecoveryState.cs
+++ BirdWarsTest/States/PasswordRecoveryState.cs
@@ -17,6 +17,7 @@ namespace BirdWarsTest.States
 			base( newContent, ref newGraphics, width_in, height_in )
 		{
 			gameObjects = new List< GameObject >();
+			gameWindow = gameWindowIn;
 		}
 
 		public override void Init( StateHandler handler ) 
@@ -26,6 +27,41 @@ namespace BirdWarsTest.States
 										 Identifiers.Background, new Vector2( 0.0f, 0.0f ) ) );
 			gameObjects.Add( new GameObject( new MenuBoxGraphicsComponent( content ), null,
 										 Identifiers.PasswordBox, new Vector2( 0.0f, 0.0f ) ) );
+			gameObjects.Add( new GameObject( new TextGraphicsComponent( content, "Recover Password" ),
+							                 null, Identifiers.TextGraphics, stateWidth, 50.0f ) );
+			gameObjects.Add( new GameObject( new TextGraphicsComponent( content, "Email" ),
+											 null, Identifiers.TextGraphics, stateWidth, 
+											 gameObjects[ 2 ].position.Y + 50.0f ) );
+			gameObjects.Add( new GameObject( new TextAreaGraphicsComponent( content ),
+											 new TextAreaInputComponent( gameWindow ),
+										     Identifiers.TextArea, stateWidth,
+										     gameObjects[ 3 ].position.Y + 20 ) );
+			gameObjects.Add( new GameObject( new Button1GraphicsComponent( content, "Send Code" ),
+											 null, Identifiers.Button1, stateWidth, 
+										     gameObjects[ 4 ].position.Y + 40.0f ) );
+			gameObjects.Add( new GameObject( new TextGraphicsComponent( content, "Code" ),
+											 null, Identifiers.TextGraphics, stateWidth,
+											 gameObjects[ 5 ].position.Y + 60.0f ) );
+			gameObjects.Add( new GameObject( new TextAreaGraphicsComponent( content ),
+											 new TextAreaInputComponent( gameWindow ),
+											 Identifiers.TextArea, stateWidth,
+											 gameObjects[ 6 ].position.Y + 20 ) );
+			gameObjects.Add( new GameObject( new TextGraphicsComponent( content, "New Password" ),
+											 null, Identifiers.TextGraphics, stateWidth,
+											 gameObjects[ 7 ].position.Y + 35.0f ) );
+			gameObjects.Add( new GameObject( new TextAreaGraphicsComponent( content ),
+											 new TextAreaInputComponent( gameWindow ),
+											 Identifiers.TextArea, stateWidth,
+											 gameObjects[ 8 ].position.Y + 20 ) );
+			gameObjects.Add( new GameObject( new Button1GraphicsComponent( content, "Reset" ),
+											 null, Identifiers.Button1, 
+											 new Vector2( gameObjects[ 9 ].position.X,
+														  gameObjects[ 9 ].position.Y + 50.0f ) ) );
+			gameObjects.Add( new GameObject( new Button1GraphicsComponent( content, "Cancel" ),
+											 new ChangeStateInputComponent( handler, StateTypes.LoginState ),
+										     Identifiers.Button1, 
+											 new Vector2( gameObjects[ 10 ].position.X + 130.0f,
+												          gameObjects[ 9 ].position.Y + 50.0f ) ) );
 		}
 
 		public override void Pause() {}
@@ -47,5 +83,6 @@ namespace BirdWarsTest.States
 		}
 
 		private List< GameObject > gameObjects;
+		private GameWindow gameWindow;
 	}
 }
