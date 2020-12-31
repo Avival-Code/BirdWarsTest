cabbcecf85cf03e8b6e832f5d8d0193fd3577ff0
Avival <31579334+Avival-Code@users.noreply.github.com>
2020-12-31 15:58:09 -0600

    Fixed ExternalPlayerAttackComponent. The method GetAttackRectangle() returned erroneous rectangle. Proper rectangle is now returned.

--- BirdWarsTest/AttackComponents/ExternalPlayerAttackComponent.cs
+++ BirdWarsTest/AttackComponents/ExternalPlayerAttackComponent.cs
@@ -24,21 +24,21 @@ namespace BirdWarsTest.AttackComponents
 			if( gameObject.Attack.IsAttacking &&
 				( ( ExternalPlayerInputComponent )gameObject.Input ).LastActiveVelocity == new Vector2( 0.0f, 1.0f ) )
 			{
-				attackRectangle = new Rectangle( ( int )gameObject.Position.X, ( int )gameObject.Position.Y - attackHeight,
+				attackRectangle = new Rectangle( ( int )gameObject.Position.X, ( int )gameObject.Position.Y + attackHeight,
 												 attackWidth, attackHeight );
 			}
 
 			if( gameObject.Attack.IsAttacking &&
 				( ( ExternalPlayerInputComponent )gameObject.Input ).LastActiveVelocity == new Vector2( -1.0f, 0.0f ) )
 			{
-				attackRectangle = new Rectangle( ( int )gameObject.Position.X, ( int )gameObject.Position.Y - attackHeight,
+				attackRectangle = new Rectangle( ( int )gameObject.Position.X - attackWidth, ( int )gameObject.Position.Y,
 												 attackWidth, attackHeight );
 			}
 
 			if( gameObject.Attack.IsAttacking &&
-				( ( ExternalPlayerInputComponent )gameObject.Input ).LastActiveVelocity == new Vector2( 1.0f, -1.0f ) )
+				( ( ExternalPlayerInputComponent )gameObject.Input ).LastActiveVelocity == new Vector2( 1.0f, 0.0f ) )
 			{
-				attackRectangle = new Rectangle( ( int )gameObject.Position.X, ( int )gameObject.Position.Y - attackHeight,
+				attackRectangle = new Rectangle( ( int )gameObject.Position.X + attackWidth, ( int )gameObject.Position.Y,
 												 attackWidth, attackHeight );
 			}
 
