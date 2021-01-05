fef92f22a1467d396984a0d395ad51070742c497
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-01-05 01:15:35 -0600

    Removed unused menu options for client and server in MainMenuState class.

--- BirdWarsTest/States/MainMenuState.cs
+++ BirdWarsTest/States/MainMenuState.cs
@@ -31,25 +31,7 @@ namespace BirdWarsTest.States
 											 null, Identifiers.Decoration, new Vector2( 0.0f, 60.0f ) ) );
 			gameObjects.Add( new GameObject( new DecorationGraphicsComponent( Content, "Logos/BirdWarsLogo_440x246" ),
 											 null, Identifiers.Decoration, new Vector2( 0.0f, 20.0f ) ) );
-			gameObjects.Add( new GameObject( new MenuOptionGraphicsComponent( Content, stringManager.GetString( StringNames.FindGame ) ), 
-										     new FindGameInputComponent(), Identifiers.MenuOption,
-											 new Vector2( 0.0f, gameObjects[ 2 ].Position.Y + 270.0f ) ) );
-			gameObjects.Add( new GameObject( new MenuOptionGraphicsComponent( Content, stringManager.GetString( StringNames.CreateGame ) ), 
-											 new CreateLobbyInputComponent(), Identifiers.MenuOption,
-											 new Vector2( 0.0f, gameObjects[ 3 ].Position.Y + 60.0f ) ) );
-			gameObjects.Add( new GameObject( new MenuOptionGraphicsComponent( Content, stringManager.GetString( StringNames.Statistics ) ), 
-										     new SelectorChangeStateInputComponent( handler, StateTypes.StatisticsState ), 
-											 Identifiers.MenuOption,
-											 new Vector2( 0.0f, gameObjects[ 4 ].Position.Y + 60.0f ) ) );
-			gameObjects.Add( new GameObject( new MenuOptionGraphicsComponent( Content, stringManager.GetString( StringNames.Settings ) ), 
-											 new SelectorChangeStateInputComponent( handler, StateTypes.OptionsState ), 
-											 Identifiers.MenuOption,
-											 new Vector2( 0.0f, gameObjects[ 5 ].Position.Y + 60.0f ) ) );
-			gameObjects.Add( new GameObject( new MenuOptionGraphicsComponent( Content, stringManager.GetString( StringNames.Logout ) ), 
-											 new LogoutInputComponent( handler ), Identifiers.MenuOption,
-											 new Vector2( 0.0f, gameObjects[ 6 ].Position.Y + 60.0f ) ) );
-			gameObjects.Add( new GameObject( null, new SelectorInputComponent( GetMenuOptions() ), Identifiers.Selector,
-											 new Vector2( 0.0f, 0.0f ) ) );
+			InitializeMenuOptions( handler, stringManager );
 		}
 
 		public override void Pause() { }
@@ -61,12 +43,12 @@ namespace BirdWarsTest.States
 			gameObjects.Clear();
 		}
 
-		public override void HandleInput(KeyboardState state) { }
+		public override void HandleInput( KeyboardState state ) { }
 
 		public override void UpdateLogic( StateHandler handler, KeyboardState state )
 		{
 			networkManager.ProcessMessages( handler );
-			gameObjects[ 8 ].Update( state, this );
+			gameObjects[ 7 ].Update( state, this );
 		}
 
 		public override void UpdateLogic( StateHandler handler, KeyboardState state, GameTime gameTime )
@@ -74,15 +56,65 @@ namespace BirdWarsTest.States
 			UpdateLogic( handler, state );
 		}
 
-		public override void Render(ref SpriteBatch sprites)
+		public override void Render( ref SpriteBatch sprites )
 		{
 			foreach( var objects in gameObjects )
-				objects.Render(ref sprites);
+			{
+				objects.Render( ref sprites );
+			}
 		}
 
-		private List<GameObject> GetMenuOptions()
+		private void InitializeMenuOptions( StateHandler handler, StringManager stringManager )
 		{
-			List<GameObject> menuOptions = new List<GameObject>();
+			if( networkManager.IsHost() )
+			{
+				InitializeHostMenuOptions( handler, stringManager );
+			}
+			else
+			{
+				InitializeClientMenuOptions( handler, stringManager );
+			}
+		}
+
+		private void InitializeHostMenuOptions( StateHandler handler, StringManager stringManager )
+		{
+			gameObjects.Add( new GameObject( new MenuOptionGraphicsComponent( Content, stringManager.GetString( StringNames.CreateGame ) ),
+											 new CreateLobbyInputComponent(), Identifiers.MenuOption,
+											 new Vector2( 0.0f, gameObjects[ 2 ].Position.Y + 270.0f ) ) );
+			gameObjects.Add( new GameObject( new MenuOptionGraphicsComponent( Content, stringManager.GetString( StringNames.Statistics ) ),
+											 new SelectorChangeStateInputComponent( handler, StateTypes.StatisticsState ),
+											 Identifiers.MenuOption, new Vector2( 0.0f, gameObjects[ 3 ].Position.Y + 60.0f ) ) );
+			gameObjects.Add( new GameObject( new MenuOptionGraphicsComponent( Content, stringManager.GetString( StringNames.Settings ) ),
+											 new SelectorChangeStateInputComponent( handler, StateTypes.OptionsState ),
+											 Identifiers.MenuOption, new Vector2( 0.0f, gameObjects[ 4 ].Position.Y + 60.0f ) ) );
+			gameObjects.Add( new GameObject( new MenuOptionGraphicsComponent(Content, stringManager.GetString( StringNames.Logout ) ),
+											 new LogoutInputComponent( handler ), Identifiers.MenuOption,
+											 new Vector2( 0.0f, gameObjects[ 5 ].Position.Y + 60.0f ) ) );
+			gameObjects.Add( new GameObject( null, new SelectorInputComponent( GetMenuOptions() ), Identifiers.Selector,
+											 new Vector2( 0.0f, 0.0f ) ) );
+		}
+
+		private void InitializeClientMenuOptions( StateHandler handler, StringManager stringManager )
+		{
+			gameObjects.Add( new GameObject( new MenuOptionGraphicsComponent( Content, stringManager.GetString( StringNames.FindGame ) ),
+											 new FindGameInputComponent(), Identifiers.MenuOption,
+											 new Vector2( 0.0f, gameObjects[ 2 ].Position.Y + 270.0f ) ) );
+			gameObjects.Add( new GameObject( new MenuOptionGraphicsComponent( Content, stringManager.GetString( StringNames.Statistics ) ),
+											 new SelectorChangeStateInputComponent( handler, StateTypes.StatisticsState ),
+											 Identifiers.MenuOption, new Vector2( 0.0f, gameObjects[ 3 ].Position.Y + 60.0f ) ) );
+			gameObjects.Add( new GameObject( new MenuOptionGraphicsComponent( Content, stringManager.GetString( StringNames.Settings ) ),
+											 new SelectorChangeStateInputComponent( handler, StateTypes.OptionsState ),
+											 Identifiers.MenuOption, new Vector2( 0.0f, gameObjects[ 4 ].Position.Y + 60.0f ) ) );
+			gameObjects.Add( new GameObject( new MenuOptionGraphicsComponent( Content, stringManager.GetString( StringNames.Logout ) ),
+											 new LogoutInputComponent( handler ), Identifiers.MenuOption,
+											 new Vector2( 0.0f, gameObjects[ 5 ].Position.Y + 60.0f ) ) );
+			gameObjects.Add( new GameObject( null, new SelectorInputComponent( GetMenuOptions() ), Identifiers.Selector,
+											 new Vector2( 0.0f, 0.0f ) ) );
+		}
+
+		private List< GameObject > GetMenuOptions()
+		{
+			List<GameObject> menuOptions = new List< GameObject >();
 			foreach( var objects in gameObjects )
 			{
 				if( objects.Identifier == Identifiers.MenuOption )
