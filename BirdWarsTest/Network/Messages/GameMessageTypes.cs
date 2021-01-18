d97a4e64c87cb9fdbd3085cfe1dffa0cb312b543
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-01-17 19:38:28 -0600

    Added comments to all game message type enumeration class.

--- BirdWarsTest/Network/Messages/GameMessageTypes.cs
+++ BirdWarsTest/Network/Messages/GameMessageTypes.cs
@@ -13,123 +13,123 @@ namespace BirdWarsTest.Network.Messages
 	public enum GameMessageTypes
 	{
 		/// <summary>
-		/// 
+		/// Chat message value
 		/// </summary>
 		ChatMessage,
 		/// <summary>
-		/// 
+		/// Join Round Request message value
 		/// </summary>
 		JoinRoundRequestMessage,
 		/// <summary>
-		/// 
+		/// Join Round Request Result message value
 		/// </summary>
 		JoinRoundRequestResultMessage,
 		/// <summary>
-		/// 
+		/// Login Request message value
 		/// </summary>
 		LoginRequestMessage,
 		/// <summary>
-		/// 
+		/// Login Result message value
 		/// </summary>
 		LoginResultMessage,
 		/// <summary>
-		/// 
+		/// Leave Round message value
 		/// </summary>
 		LeaveRoundMessage,
 		/// <summary>
-		/// 
+		/// Register user message value
 		/// </summary>
 		RegisterUserMessage,
 		/// <summary>
-		/// 
+		/// Round created message value
 		/// </summary>
 		RoundCreatedMessage,
 		/// <summary>
-		/// 
+		/// Round state changed message value
 		/// </summary>
 		RoundStateChangedMessage,
 		/// <summary>
-		/// 
+		/// Password reset message value
 		/// </summary>
 		PasswordResetMessage,
 		/// <summary>
-		/// 
+		/// Password Reset result message value
 		/// </summary>
 		PasswordResetResultMessage,
 		/// <summary>
-		/// 
+		/// Solicit password reset message value
 		/// </summary>
 		SolicitPasswordResetMessage,
 		/// <summary>
-		/// 
+		/// Solicit password result message value
 		/// </summary>
 		SolicitPasswordResultMessage,
 		/// <summary>
-		/// 
+		/// Start round message value
 		/// </summary>
 		StartRoundMessage,
 		/// <summary>
-		/// 
+		/// Player state change message value
 		/// </summary>
 		PlayerStateChangeMessage,
 		/// <summary>
-		/// 
+		/// Spawn box message value
 		/// </summary>
 		SpawnBoxMessage,
 		/// <summary>
-		/// 
+		/// Box damage message value
 		/// </summary>
 		BoxDamageMessage,
 		/// <summary>
-		/// 
+		/// Player attack message value
 		/// </summary>
 		PlayerAttackMessage,
 		/// <summary>
-		/// 
+		/// Spawn consumables message value
 		/// </summary>
 		SpawnConsumablesMessage,
 		/// <summary>
-		/// 
+		/// Picked up item message value
 		/// </summary>
 		PickedUpItemMessage,
 		/// <summary>
-		/// 
+		/// Spawn Grenade message value
 		/// </summary>
 		SpawnGrenadeMessage,
 		/// <summary>
-		/// 
+		/// Update Round Time message value
 		/// </summary>
 		UpdateRoundTimeMessage,
 		/// <summary>
-		/// 
+		/// Update User statistics message value
 		/// </summary>
 		UpdateUserStatisticsMessage,
 		/// <summary>
-		/// 
+		/// Round Finished message value
 		/// </summary>
 		RoundFinishedMessage,
 		/// <summary>
-		/// 
+		/// Registration result message value
 		/// </summary>
 		RegistrationResultMessage,
 		/// <summary>
-		/// 
+		/// Player is dead message value
 		/// </summary>
 		PlayerIsDeadMessage,
 		/// <summary>
-		/// 
+		/// Exit waiting room message value
 		/// </summary>
 		ExitWaitingRoomMessage,
 		/// <summary>
-		/// 
+		/// Test connection message value
 		/// </summary>
 		TestConnectionMessage,
 		/// <summary>
-		/// 
+		/// Ban player message value
 		/// </summary>
 		BanPlayerMessage,
 		/// <summary>
-		/// 
+		/// Adjusted player state change message value
 		/// </summary>
 		AdjustedPlayerStateChangeMessage
 	}
